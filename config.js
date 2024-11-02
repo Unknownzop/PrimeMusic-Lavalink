fb304f3e11e1412f1781630c144556471de00fa8
Unknownz <142665329+Unknownzop@users.noreply.github.com>
2024-11-02 17:54:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -39,9 +39,9 @@ module.exports = {
   nodes: [
     {
         name: "Server",
-        host: "nodev4.fypmoon.org",
-        port: 1118,
-        password: "fypmoon.org",
+        host: "lavalink.jirayu.net",
+        port: 2334,
+        password: "youshallnotpass",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
